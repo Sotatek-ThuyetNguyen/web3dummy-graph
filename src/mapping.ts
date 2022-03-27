6fbef1428b02f4b6e9d0c8ead7f385fa9211974c
Sotatek-ThuyetNguyen <thuyet.nguyen@sotatek.com>
2022-03-27 11:40:56 +0700

    Just me, I'm 18+

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,28 +6,25 @@ import {
   OwnershipTransferred,
   Withdraw
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Ahihi } from "../generated/schema"
 
 export function handleDeposit(event: Deposit): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Ahihi.load(event.transaction.hash.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new Ahihi(event.transaction.from.toHex())
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.user = event.params.user
-  entity.amount = event.params.amount
+  // entity.user = event.params.user
+  entity.user = event.params.user.toHex();
+  entity.amount = event.params.amount;
+  entity.block = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.type = 'Deposit';
 
   // Entities can be written to the store with `.save()`
   entity.save()
@@ -60,4 +57,22 @@ export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handleWithdraw(event: Withdraw): void {}
+export function handleWithdraw(event: Withdraw): void {
+  let entity = Ahihi.load(event.transaction.hash.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new Ahihi(event.transaction.from.toHex())
+  }
+
+  // entity.user = event.params.user
+  entity.user = event.params.user.toHex();
+  entity.amount = event.params.amount;
+  entity.block = event.block.number;
+  entity.timestamp = event.block.timestamp;
+  entity.type = 'Withdraw';
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+}
